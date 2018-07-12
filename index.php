d319d6de66f9dec179f1124194e7119e97a93031
Elias Roberts <eliroberts9@gmail.com>
2018-07-12 08:26:17 -0600

    added use case and frustrations for John to index.php

--- index.php
+++ index.php
@@ -18,10 +18,19 @@
 			</ul>
 			<li>Devices/Technical Knowledge</li>
 			<ul>
-				<li>John is moderately tech savvy although he only listens to vinyl and has limited knowledge of non-Mac
+				<li>John is comfortable using most tech although he only listens to vinyl and has limited knowledge of non-Mac
 				products.</li>
 				<li>MacBook Air 13.3", 256GB storage, 1.6 GHz dual-core Intel Core i5, 1440 x 900 resolution</li>
 				<li>iPhone X, 5.8" screen, 256GB storage, 2436 x 1125 resolution.</li>
+			</ul>
+			<li>Use Case</li>
+			<ul>John would like to easily be able to publibly express his opinions about and rate concert venues he goes
+				to.
+			</ul>
+			<li>Frustrations</li>
+			<ul>
+				<li>Poor performance of mobile sites/apps.</li>
+				<li>Vetting period for posting comments/reviews.</li>
 			</ul>
 		</ul>
 	</body>
