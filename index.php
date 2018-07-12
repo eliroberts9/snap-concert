96acab624b9c3b295a2c1d6f9f36801cc8133571
Elias Roberts <eliroberts9@gmail.com>
2018-07-12 08:19:26 -0600

    created index.php, added details about person to index.php, added image of persona

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Snap Concert</title>
+	</head>
+	<body>
+		<h1>John Hinkle</h1>
+		<img src="./hipster.jpeg">
+		<ul>
+			<li>Persona</li>
+			<ul>
+				<li>Age: 28</li>
+				<li>Male</li>
+				<li>Barista</li>
+				<li>John is highly opinionated.  He believes any music which is played on the radio is garbage.  He will
+				readily look down upon anyone who thinks otherwise.</li>
+			</ul>
+			<li>Devices/Technical Knowledge</li>
+			<ul>
+				<li>John is moderately tech savvy although he only listens to vinyl and has limited knowledge of non-Mac
+				products.</li>
+				<li>MacBook Air 13.3", 256GB storage, 1.6 GHz dual-core Intel Core i5, 1440 x 900 resolution</li>
+				<li>iPhone X, 5.8" screen, 256GB storage, 2436 x 1125 resolution.</li>
+			</ul>
+		</ul>
+	</body>
+</html>
