bb4340e2c84ea6dc3d572bb97078babec04f48fc
Elias Roberts <eliroberts9@gmail.com>
2018-07-12 08:51:37 -0600

    added spacing to html

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
 		<link rel="stylesheet" type="text/css" href="./styles.css" />
 	</head>
 	<body>
+
 		<h1>John Hinkle</h1>
 		<img src="./hipster.jpeg" />
+		
 		<ul>
 			<li class="title">Persona</li>
 			<ul>
@@ -17,6 +19,7 @@
 				<li>John is highly opinionated.  He believes any music which is played on the radio is garbage.  He will
 				readily look down upon anyone who thinks otherwise.</li>
 			</ul>
+
 			<li class="title">Devices/Technical Knowledge</li>
 			<ul>
 				<li>John is comfortable using most tech although he only listens to vinyl and has limited knowledge of non-Mac
@@ -24,15 +27,18 @@
 				<li>MacBook Air 13.3", 256GB storage, 1.6 GHz dual-core Intel Core i5, 1440 x 900 resolution</li>
 				<li>iPhone X, 5.8" screen, 256GB storage, 2436 x 1125 resolution.</li>
 			</ul>
+
 			<li class="title">Use Case</li>
 			<ul>John would like to easily be able to publicly express his opinions about and rate concert venues he goes
 				to as well as save favorites and share with friends.
 			</ul>
+
 			<li class="title">Frustrations</li>
 			<ul>
 				<li>Poor performance of mobile sites/apps.</li>
 				<li>Vetting period for posting comments/reviews.</li>
 			</ul>
+
 			<li class="title">Interaction Flow</li>
 			<ul>
 				<li>John opens mobile site on his phone and searches for venue.</li>
